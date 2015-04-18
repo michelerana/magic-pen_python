fad22efb827371ba046007622a6ca953a648aceb
Mario Lamacchia <mariolamacchia@gmail.com>
2015-04-18 11:08:37 +0200

    Make code PEP8 compliant

--- pen_person.py
+++ pen_person.py
@@ -2,9 +2,11 @@ class Pen(object):
     def __init__(self):
         self.color = "black"
 
+
 class NormalPen(Pen):
     def __init__(self):
         Pen.__init__(self)
+
 
 class MagicPen(Pen):
     magicOn = 1
@@ -14,16 +16,16 @@ class MagicPen(Pen):
         Pen.__init__(self)
 
     def getText(self, text):
-        if MagicPen.magicOn != 0: 
+        if MagicPen.magicOn != 0:
             return self.magicText
         else:
             return text
 
+
 class Person(object):
     def __init__(self, name):
         self.name = name
-    
+
     def write(self, pen, text):
         pen.text = text
-	print pen.text
-
+        print pen.text
